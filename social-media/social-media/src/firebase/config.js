b91570bee5aed4522d958932379532780c3cd428
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-03-09 20:18:52 +0000

    Update config.js

--- social-media/social-media/src/firebase/config.js
+++ social-media/social-media/src/firebase/config.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8nLA8Iw_YB4MLalupHeDKVKQCvuRjxU0",
-  authDomain: "social-media-1effc.firebaseapp.com",
-  projectId: "social-media-1effc",
-  storageBucket: "social-media-1effc.appspot.com",
-  messagingSenderId: "226875517401",
-  appId: "1:226875517401:web:53ad77b696cbaf6974d1c8",
+  apiKey: "AIzaSyDYFA5AcGHO87WCV70rdkvmEk0fRvsPX18",
+  authDomain: "socialmedia-6a09e.firebaseapp.com",
+  projectId: "socialmedia-6a09e",
+  storageBucket: "socialmedia-6a09e.appspot.com",
+  messagingSenderId: "735152795701",
+  appId: "1:735152795701:web:10a1dbdf600434e674c17c"
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
