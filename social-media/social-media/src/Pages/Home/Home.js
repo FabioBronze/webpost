364f1a86b114030e52bb3c904790599cfd60aacb
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-04-26 02:22:25 +0100

    home component

--- social-media/social-media/src/Pages/Home/Home.js
+++ social-media/social-media/src/Pages/Home/Home.js
@@ -26,23 +26,23 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
-      <h1>Veja as postagens mais recentes</h1>
+      <h1>See our most recent posts!</h1>
       <form className={styles.search} onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="ou pesquise por Tags..."
+          placeholder="Search tags"
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button className="btn btn-dark">Pesquisar</button>
+        <button className="btn btn-dark">Search</button>
       </form>
       <div>
-        {loading && <p>Carregando...</p>}
+        {loading && <p>Loading...</p>}
         {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
-            <p>Não foram encontradas Postagens</p>
+            <p>No posts found...</p>
             <Link to="/posts/create" className="btn">
-              Criar primeira Postagem
+              Create first post
             </Link>
           </div>
         )}
